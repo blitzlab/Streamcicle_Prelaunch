f5140f37f9acec3dec48f97c080a6b8a65d26e7d
maryyam <marryam.sedghi@gmail.com>
2021-03-25 20:11:00 -0600

    Delete streamcicle_prelaunch/streamcicle_prelaunch directory

--- streamcicle_prelaunch/streamcicle_prelaunch/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-from django.views.generic.base import TemplateView
-
-# Current pages under the scope of accounts
-
-urlpatterns = [
-    path('', views.signup, name='signup' )]
